4db32b1bd6204af4814aca244bd799f8f91d5e69
ProfMara <94469709+ProfMara@users.noreply.github.com>
2022-03-14 16:10:07 -0300

    Add files via upload

---
+++ ATIVIDADE 1/sketch.js
@@ -0,0 +1,24 @@
+var notas = [60, 30, 89, 87, 86, 54, 59, 96, 100, 85, 64, 73, 62, 35, 31];
+
+
+function setup() {
+    //cria a área de jogo
+    createCanvas(800, 400);
+
+    for (var contador = 0; contador < ; contador++) {
+        if ( >= 60) {
+            console.log(+" APROVADO");
+        }
+    }
+
+
+}
+
+function draw() {
+    background("black");
+
+
+
+    drawSprites();
+
+}
